c0a2d35794d12fd8809c918f75a2f5c5bc0dc1e2
MadelineWilliams <madeline.williams0110@gmail.com>
2026-01-16 23:52:14 +0500

    Add option to save trained model

--- energy_forecast_uci_lstm.py
+++ energy_forecast_uci_lstm.py
@@ -2,7 +2,7 @@
 # Author: Madeeha Rehan
 # Description: Predicts future energy consumption using time-series data
 
-# Step 1: Import libraries.
+# Step 1: Import libraries..
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
